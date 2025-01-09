2578c67f3698f41e9f97827ec2a57e8fa0b3acae
Saym Islam Jihad <kojn6193@gmail.com>
2025-01-09 17:44:24 +0600

    Added error handling in many ways

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const Listings = require('./models/listing.js');
 const exp = require('constants');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-
+const wrapAsync = require('./utils/WrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+const listingSchema = require('./schema.js');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,58 +25,84 @@ main().then(()=>{
 })
 
 
+const validateSchema = (req, res, next)=>{
+    let {error} =  listingSchema.validate(req.body);
+    if(error){
+        throw new ExpressError(400, error);
+    }else{
+        next();
+    }
+}
+
 app.get('/', (req, res)=>{
     res.send("Hi i am root");
 })
 
 // index route start here
-app.get('/listings', async (req, res)=>{
+app.get('/listings', wrapAsync(async (req, res)=>{
     const listings = await Listings.find();
     // console.log(listings);
     res.render('./listings/index.ejs', {listings})
-})
+}))
 
-
+// new route
 app.get('/listings/new', (req, res)=>{
     res.render('./listings/create.ejs');
 })
 
 // show route
-app.get('/listings/:id', async (req, res)=>{
+app.get('/listings/:id', wrapAsync(async (req, res)=>{
     let {id} = req.params;
     const listing = await Listings.findById(id);
     res.render('./listings/show.ejs', {listing});
-})
+}))
 
-app.post('/listings', async (req, res)=>{
+// create route
+app.post('/listings',validateSchema, wrapAsync(async (req, res)=>{
+    // let result = listingSchema.validate(req.body);
+    // // console.log(result);
+    // if(result.error){
+    //     throw new ExpressError(400, result.error);
+    // }
     const newListing = new Listings(req.body.listing);
     await newListing.save();
     res.redirect('/listings');
-})
+}))
 
-app.get('/listing/:id/edit', async (req, res)=>{
+// edit route
+app.get('/listing/:id/edit', wrapAsync(async (req, res)=>{
     let {id} = req.params;
     const listing = await Listings.findById(id);
     res.render('./listings/edit.ejs', {listing});
-})
+}))
 
-app.put('/listings/:id',async (req, res)=>{
+app.put('/listings/:id', validateSchema, wrapAsync(async (req, res)=>{
     let {id} = req.params;
     await Listings.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}))
 
-app.delete('/listings/:id', async (req, res)=>{
+// delete route
+app.delete('/listings/:id', wrapAsync(async (req, res)=>{
     let {id} = req.params;
     let deletedValue = await Listings.findByIdAndDelete(id);
     console.log(deletedValue);
     res.redirect("/listings")
-});
+}));
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
+
+app.all("*", (req, res, next)=>{
+    next(new ExpressError(404, "Page Not Found!"));
+})
+app.use((err, req, res, next)=>{
+    let {StatusCode = 500, message="Some error happend"} = err;
+    res.status(StatusCode).render('error.ejs', {StatusCode, message});
+    // res.status(StatusCode).send(message);
+})
 const port = 8080;
 app.listen(port, ()=>{
     console.log(`App started listening on port ${port}`);
